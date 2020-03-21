447b8fee4e50321fe64a7d43cbed2ad09266e45a
DestinyV <37183101+DestinyV@users.noreply.github.com>
2020-03-21 13:35:53 +0800

    :star2: login背景添加自适应缩放样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,7 +4,14 @@
   height: 100%;
   .login-bg {
     display: block;
-    object-fit: contain;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    margin: auto;
   }
 }
 </style>
